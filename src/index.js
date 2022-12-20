8627176abe54168e48e7f9b3c64cdfa209c46417
maestro_roshi <ccdelgadop@gmail.com>
2022-12-20 14:38:28 -0500

    clean code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const fs = require("fs")
 const { parse } = require("csv-parse")
 const run = require("./mongoDb.js")
-console.log('run: ', run);
 
 const data = []
 
@@ -20,21 +19,8 @@ fs.createReadStream("./src/dispositivosMedicosStent.csv")
     console.log(err);
   })
 
-
-const cleanFile = (row) => {
-  if (row.precioVentaBase && row.precioVentaBase.includes("parametrizar")){
-    return
-  }
-
-  if (row["TARIFA SAVIA SALUD"] && row["TARIFA SAVIA SALUD"].includes("parametrizar")){
-    return 
-  }
-
-  return row
-}
-
 async function updateRow(data) {
-  console.log('data: ', data.length);
+  console.log('Total registros a registrar: ', data.length);
   // Add connection
   const client = await run();
   const database = client.db('hisPruebasDb');
@@ -44,22 +30,28 @@ async function updateRow(data) {
   for (let i = 0; i < data.length; i++) {
     if(data[i]._id) {
       const register = await dispositivosMedicos.findOne({ _id: data[i]._id })
-      if(register.precioVentaBase != data[i].precioVentaBase){
-        console.log("!=", data[i]._id, data[i].precioVentaBase, register.precioVentaBase);
-      }
+      let newDescripcion = ""
+      let newPrecioBase = 0
       // update precioVentaBase
-      if(data[i].precioVentaBase) {
-        register.precioVentaBase = +data[i].precioVentaBase
+      if(data[i].precioVentaBase && (register.precioVentaBase != data[i].precioVentaBase)) {
+        newPrecioBase = +data[i].precioVentaBase
       }
       // update descripción
-      if(data[i].descripcion) {
-        register.descripcion = data[i].descripcion
+      if(data[i].descripcion && (register.descripcion != data[i].descripcion)) {
+        newDescripcion = data[i].descripcion
       }
 
-      if(data[i]._id == "WgzbPtzJXLv368Nok"){
+      if(newDescripcion || newPrecioBase){
+
+        const newDevice = {
+          ...register,
+          precioVentaBase: newPrecioBase || data[i].precioVentaBase,
+          descripcion: newDescripcion || data[i].descripcion
+        }
+
         await dispositivosMedicos.updateOne(
           { _id: data[i]._id },
-          { $set: register }
+          { $set: newDevice }
         )
       }
 
@@ -69,6 +61,7 @@ async function updateRow(data) {
     }
   }
 
+  console.log("Total Registered", data.length - notExists)
   console.log("Total no exists", notExists);
 
   // close connection
--- src/mongoDb.js
+++ src/mongoDb.js
@@ -8,18 +8,10 @@ async function run() {
   try {
     if(!client) {
       client = new MongoClient(uri);
-      console.log("*New client*");
     }
     return client;
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await dispositivosMedicos.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    console.log("priii");
-    // await client.close();
+  } catch (err) {
+    await client.close()
   }
 }
 
